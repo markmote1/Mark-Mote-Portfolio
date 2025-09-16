a195f19d339f76fb73c4a0651992126df6a779c9
Mark Mote <137040517+markmote1@users.noreply.github.com>
2025-09-16 15:01:22 +0300

    Added Contact form

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+
+//floating about page
 document.getElementById("openAbout").onclick = openAbout;
 function openAbout() {
-    var w = window.open("about.html",
-    "", "width=200, height=300,left=300,top=400");
+    var w = window.open("about.html","", "width=200, height=300,left=300,top=400");
+    
 
 }
